3884daa9c0d52f864563224ee593e32401d7b1a6
seetha-142 <seethadk86@gmail.com>
2024-01-26 18:02:29 +0530

    new prgrms

--- IP LAB/negative.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import numpy as np
-image=Image.open('img1.jpeg')
-image_array=255-np.array(image)
-negative_image=Image.fromarray(image_array)
-negative_image.show()
-negative_image.save('negative_image.jpeg')
-
-
